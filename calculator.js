76d32b11418487f5d907504bf3bd6278ac357864
Isnendyan <Isnendyankp@gmail.com>
2025-03-08 09:50:43 +0700

    docs: Add explanatory comments to init function

---
+++ calculator.js
@@ -0,0 +1,13 @@
+// Fungsi init() digunakan untuk menginisialisasi kalkulator
+// Fungsi ini akan dijalankan saat halaman pertama kali dimuat
+function init() {
+    // Mencari elemen dengan class 'calc-buttons' yang berisi semua tombol kalkulator
+    document
+      .querySelector(".calc-buttons")
+      // Menambahkan event listener untuk menangkap klik pada area tombol
+      .addEventListener("click", function (event) {
+        // Ketika tombol diklik, ambil teks dari tombol (misal: "1", "+", "=")
+        // dan kirim ke fungsi buttonClick untuk diproses
+        buttonClick(event.target.innerText);
+      });
+  }
